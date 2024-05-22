aa3366e9207784d3b9a1a9cece8c19bf4779867f
Mindypupu <wushi0217@gmail.com>
2024-05-22 20:21:35 +0800

    add CartItem

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,20 +1,66 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingCart {
     //Pizza class
-    private Map<Pizza, Integer> pizzas;
+    private List<CartItem> items;
     private double discountPercentage;
 
-    public ShoppingCart(){
-        pizzas = new HashMap<>();
+    public ShoppingCart() {
+        items = new ArrayList<>();
         discountPercentage = 1.0;
     }
-    public void addPizza(Pizza newPizza, int quantity){
-        pizzas.merge(newPizza, quantity, Integer::sum);
+
+    public void changePizza(Pizza pizza, String size, int quantity, Pizza newPizza){
+        for(CartItem item : items){
+            //exist
+            if(item.getPizza().equals(pizza) && item.getSize().equals(size) && item.getQuantity() == (quantity)){
+                item.setPizza(newPizza);
+                return;
+            }
+        }
+        System.out.println("The item doesn't exist.");
     }
-    public void removePizza(Pizza pizza){
-        pizzas.remove(pizza);
+    public void changeSize(Pizza pizza, String size, int quantity, String newSize){
+        for(CartItem item : items){
+            //exist
+            if(item.getPizza().equals(pizza) && item.getSize().equals(size) && item.getQuantity() == (quantity)){
+                item.setSize(newSize);
+                return;
+            }
+        }
+        System.out.println("The item doesn't exist.");
+    }
+    public void changeQuantity(Pizza pizza, String size, int quantity, int newQuantity){
+        for(CartItem item : items){
+            //exist
+            if(item.getPizza().equals(pizza) && item.getSize().equals(size) && item.getQuantity() == (quantity)){
+                item.setQuantity(newQuantity);
+                return;
+            }
+        }
+        System.out.println("The item doesn't exist.");
+    }
+
+    public void addCartItem(Pizza pizza, String size, int quantity){
+        for(CartItem item : items){
+            //exist
+            if(item.getPizza().equals(pizza) && item.getSize().equals(size)){
+                item.setQuantity(item.getQuantity() + quantity);
+                return;
+            }
+        }
+        CartItem newItem = new CartItem(pizza, size, quantity);
+        items.add(newItem);
+    }
+    public void removeCartItem(Pizza pizza, String size){
+        for(CartItem item : items){
+            //exist
+            if(item.getPizza().equals(pizza) && item.getSize().equals(size)){
+                items.remove(item);
+                return;
+            }
+        }
     }
     public void applyDiscountCode(String code) {
         if (code.equals("DISCOUNT10")) {
@@ -28,12 +74,16 @@ public class ShoppingCart {
 
     public double calculateTotal(){
         double total = 0;
-        for(Map.Entry<Pizza, Integer> entry: pizzas.entrySet()){
-            total += entry.getValue() * entry.getKey().getPrice();
+        for(CartItem item : items) {
+            if (item.getSize().equals("large")) {
+                total += item.getPizza().getLargePrice() * item.getQuantity();
+            }
+            //small
+            else {
+                total += item.getPizza().getSmallPrice() * item.getQuantity();
+            }
         }
         total *= discountPercentage;
         return total;
     }
-    
 }
-
